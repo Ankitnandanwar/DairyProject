67991f3461e9f050fd434fedd10086dec298d3c8
Ankitnandanwar <ankitnandanwar3@gmail.com>
2023-12-07 13:09:02 +0530

    First Commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Login.css"
+// import loginLogo from "../../assets/companylogo.png"
+import loginLogo from "../../assets/logo.png"
+import TextField from '@mui/material/TextField';
+import {useNavigate} from 'react-router-dom'
+
+const Login = () => {
+
+  const navigate = useNavigate()
+  
+  return (
+    <>
+      <div className="backgroundImg">
+        <div className="loginCont">
+          <div className="empty"></div>
+          <div className="loginIds">
+            <div>
+              <img src={loginLogo} alt="cpmpany-logo" width="130" />
+            </div>
+            <div>
+              <form action="" class="loginFields">
+                <div className="input-box">
+                  <TextField id="standard-basic" label="Username" variant="standard" />
+                  {/* <span style={{ color: "red", fontSize: "10px", marginTop:"7px"}}>username should not be empty</span> */}
+                </div>
+                <div className="input-box">
+                  <TextField id="standard-basic" type="password" label="Password" variant="standard" />
+                  {/* <span style={{ color: "red", fontSize: "10px", marginTop:"7px"}}>password should not be empty</span> */}
+                </div>
+                <button type="submit" class="login-btn">Login</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
