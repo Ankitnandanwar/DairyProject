49da7841c6d3f1abf14ed63abe5b14a409674419
Ankitnandanwar <ankitnandanwar3@gmail.com>
2023-12-08 15:32:19 +0530

    Navbar and Sidebar done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Auth/Login'
+import DashboardPage from './pages/Dashboard/DashboardPage'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login/>} />
+          <Route path='/dashboardpage' element={<DashboardPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Dashboard from '../../components/Modules/Dashboard/Dashboard'
+
+const DashboardPage = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Dashboard/>
+    </div>
+  )
+}
+
+export default DashboardPage
