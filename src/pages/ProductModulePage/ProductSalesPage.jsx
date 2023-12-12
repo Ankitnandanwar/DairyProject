d78a2f75fbdd92948610e04bf71c712420437558
Navinfri <gurramnavin8@gmail.com>
2023-12-12 18:51:37 +0530

    product Sale

---
+++ src/pages/ProductModulePage/ProductSalesPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import ProductSale from '../../components/Modules/Product/ProductSale'
+
+const ProductSalesPage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <ProductSale/>
+    </div>
+  )
+}
+
+export default ProductSalesPage
